35955985aa898490249f1ae143f6c64a71043251
Hỏi Dân IT <jvgiveup@gmail.com>
2024-02-07 19:34:30 +0700

    video 33

--- src/main/java/vn/hoidanit/laptopshop/LaptopshopApplication.java
+++ src/main/java/vn/hoidanit/laptopshop/LaptopshopApplication.java
@@ -9,11 +9,7 @@ public class LaptopshopApplication {
 
 	public static void main(String[] args) {
 
-		// container
-		ApplicationContext hoidanit = SpringApplication.run(LaptopshopApplication.class, args);
-		for (String s : hoidanit.getBeanDefinitionNames()) {
-			System.out.println(s);
-		}
+		SpringApplication.run(LaptopshopApplication.class, args);
 
 	}
 
