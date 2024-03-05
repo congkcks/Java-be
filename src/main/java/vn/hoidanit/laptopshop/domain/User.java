6a95df890876e18cdf59da5452e3065030ca88f8
Hỏi Dân IT và Eric <haryphamdev@gmail.com>
2024-03-05 22:26:44 +0700

    video 76

--- src/main/java/vn/hoidanit/laptopshop/domain/User.java
+++ src/main/java/vn/hoidanit/laptopshop/domain/User.java
@@ -18,6 +18,10 @@ public class User {
     private String address;
     private String phone;
 
+    private String avatar;
+
+    // roleId
+
     public long getId() {
         return id;
     }
@@ -66,10 +70,18 @@ public class User {
         this.phone = phone;
     }
 
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
     @Override
     public String toString() {
         return "User [id=" + id + ", email=" + email + ", password=" + password + ", fullName=" + fullName
-                + ", address=" + address + ", phone=" + phone + "]";
+                + ", address=" + address + ", phone=" + phone + ", avatar=" + avatar + "]";
     }
 
 }
